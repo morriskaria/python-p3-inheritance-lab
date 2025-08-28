c841fe0c2250fc4b8eb2fdea4cb8cd43cb2461c6
morriskaria <morriskaria542@gmail.com>
2025-08-28 18:59:29 +0300

    commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python
-
+# lib/teacher.py
+from random import randint
 from user import User
 
-import random
-
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+    
     def teach(self):
-        pass
+        if not self.knowledge:
+            return None
+        random_index = randint(0, len(self.knowledge) - 1)
+        return self.knowledge[random_index]
